19440ff65c00dfe6106bd7254d5edb2691ffdb6a
rayssamorei <rayssa32alvess@gmail.com>
2024-02-13 16:14:29 -0300

    POO - Programação Orientada a Objetos

---
+++ src/POO/pessoa/Pessoa.java
@@ -0,0 +1,28 @@
+package POO.pessoa;
+
+public class Pessoa {
+    private int idade;
+    private String nome;
+    private double altura;
+
+    //<modificador-acesso> <retorno> nomeDoMetodo
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public double getAltura(){
+        return altura;
+    }
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+
+}
---
+++ src/POO/pessoa/Teste.java
@@ -0,0 +1,14 @@
+package POO.pessoa;
+
+public class Teste {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        p.setAltura(1.73);
+        p.setIdade(16);
+        p.setNome("Rayssa");
+
+        System.out.println("Nome: " + p.getNome());
+        System.out.println("Idade: " + p.getIdade());
+        System.out.println("Altura: " + p.getAltura());
+    }
+}
